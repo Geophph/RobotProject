892d83d43c28f1e6ee81babf04fd72c7853a9f9d
Jeffery Valli <valli.6@osu.edu>
2018-02-19 23:49:17 -0500

    Version 2 -Distance tracking added -Movement added

--- main.cpp
+++ main.cpp
@@ -4,17 +4,46 @@
 #include<FEHLCD.h>
 #include<FEHIO.h>
 #include<FEHSD.h>
+#include <math.h>
 
+//define values that are dependent on sensor used
 #define redmax 10
 #define redmin 0
 #define bluemax 2.7
 #define bluemin 2.6
+#define lefton 2.104
+#define leftoff 0.189
+#define middleon 2.320
+#define middleoff 0.398
+#define righton 2.125
+#define rightoff 0.183
+#define PI (3.141592653589793)
 
+//define each sensor or motor
 FEHMotor l_motor(FEHMotor::Motor0,9.0);
 FEHMotor r_motor(FEHMotor::Motor1,9.0);
-AnalogInputPin colorsensor(FEHIO::P3_7);
+DigitalEncoder right_encoder(FEHIO::P0_0);
+DigitalEncoder left_encoder(FEHIO::P0_1);
+AnalogInputPin colorsensor(FEHIO::P3_4);
+AnalogInputPin linel(FEHIO::P3_7);
+AnalogInputPin linem(FEHIO::P3_6);
+AnalogInputPin liner(FEHIO::P3_5);
 FEHServo lifter(FEHServo::Servo0);
 FEHServo turner(FEHServo::Servo1);
+
+int distance(int dist){
+    int travel;
+    float counts;
+    counts = (127.2/PI)*dist;
+    if((left_encoder.Counts() + (right_encoder.Counts())) / 2.0 < counts){
+        travel = 1;
+    }
+    else{
+        travel = 0;
+    }
+    return travel;
+}
+
 
 int color(void){
     int color = colorsensor.Value();
@@ -34,17 +63,52 @@ int color(void){
     return color_return;
 }
 
-void track_line(int speed, int distance){
+void track_line(int speed, int dist){
+    int state = 1;
+    float left, middle, right;
+    right_encoder.ResetCounts();
+    left_encoder.ResetCounts();
+    while(distance(dist)){
+        left=linel.Value();
+        middle=linem.Value();
+        right=liner.Value();
 
+        if(abs(lefton-left)>0.4)
+        {
+            state=2;                    //too far left
+            r_motor.SetPercent(30);
+            l_motor.SetPercent(10);
+        }
+        else if(abs(righton-right)>0.4)
+        {
+            state=3;                    //too far right
+            r_motor.SetPercent(10);
+            l_motor.SetPercent(30);
+        }
+        else
+        {
+            state=1;
+            r_motor.SetPercent(20);
+            l_motor.SetPercent(20);
+        }
+    }
+    r_motor.SetPercent(0);
+    l_motor.SetPercent(0);
 }
 
 void lift_arm(int height){
 
 }
 
-void drive(int speed, int distance){
-    r_motor.SetPercent(speed);
-    l_motor.SetPercent(speed);
+void drive(int speed, int dist){
+    right_encoder.ResetCounts();
+    left_encoder.ResetCounts();
+    while(distance(dist)){
+        r_motor.SetPercent(speed);
+        l_motor.SetPercent(speed);
+    }
+    r_motor.SetPercent(0);
+    l_motor.SetPercent(0);
 }
 
 void logrws(){
@@ -54,23 +118,21 @@ void logrws(){
 int main(void)
 {   int mode=0, run=1, run_num=0;
     float x, y;
+    //open log to track runs
     SD.OpenLog();
+    //set values min and max for servos
     lifter.SetMin(500);
     lifter.SetMax(2400);
     turner.SetMin(500);
     turner.SetMax(2400);
+    //define Icons used in menu
     FEHIcon::Icon start;
-    FEHIcon::Icon stop;
-    FEHIcon::Icon data;
-    FEHIcon::Icon back;
     FEHIcon::Icon quit;
-    LCD.Clear();
     start.SetProperties("Start", 10, 10, 80, 30,BLACK,WHITE);
-    stop.SetProperties("Stop", 10, 10, 80, 30,BLACK,WHITE);
-    data.SetProperties("Data", 10, 50, 80, 30,BLACK,WHITE);
-    back.SetProperties("Back", 10, 10, 80, 30,BLACK,WHITE);
     quit.SetProperties("Quit", 10, 50, 80, 30,BLACK,WHITE);
+    LCD.Clear();
     while(run){
+        //menu to start and quit
         while(mode==0&&run){
             int press=0;
             start.Draw();
@@ -88,16 +150,12 @@ int main(void)
                 }
             }
         }
+        //run program
         while(mode==1&&run){
-            stop.Draw();
-            int press=0;
-            while(LCD.Touch(&x,&y)&&!press){
-                if(stop.Pressed(x,y,1)){
-                press=1;
-                mode = 0;
-                LCD.Clear();
-                }
-            }
+            run_num++;
+            drive(20, 12);
+            //while(color()!=0);
+
         }
 }
     SD.CloseLog();
